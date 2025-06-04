ec6bf18e72c870afb6759f15df4d53fa6d4dfba9
Bharat-Ag <108234716+Bharat-Ag@users.noreply.github.com>
2025-06-04 18:11:04 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -43,8 +43,8 @@ export default function Sidebar() {
         <div>
             <div className="p-5 flex flex-col h-full">
                 <div className='flex items-center'>
-                    <img src={assets.mainLogo} alt="" className='max-w-10' />
-                    <span className='font-bold ml-4 text-xl'>Chit-Chat</span>
+{/*                     <img src={assets.mainLogo} alt="" className='max-w-10' /> */}
+                    <span className='font-bold ml-4 text-xl'>Chat</span>
                 </div>
                 <hr className='my-5 text-white/15' />
                 <div className='flex flex-col flex-grow-1'>
