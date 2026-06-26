a688aa74ef7b22edc82d4570e2828cb1e49d9e12
Bharat Agrawal <bharat.agrawal@pavansgroup.in>
2026-06-26 15:34:04 +0530

    api tail removed

--- config/apiRoutes.js
+++ config/apiRoutes.js
@@ -2,15 +2,15 @@ import config from "./config";
 
 const apiRoutes = {
   auth: {
-    login: config.baseUrl + "/api/auth/login",
-    signup: config.baseUrl + "/api/auth/signup",
-    checkAuth: config.baseUrl + '/api/auth/check',
-    resetPassword: config.baseUrl + '/api/auth/reset-password',
+    login: config.baseUrl + "api/auth/login",
+    signup: config.baseUrl + "api/auth/signup",
+    checkAuth: config.baseUrl + 'api/auth/check',
+    resetPassword: config.baseUrl + 'api/auth/reset-password',
   },
 
   user: {
-    updateProfile: config.baseUrl + '/auth/update-profile',
-    onlineVisiblity: config.baseUrl + '/auth/online-visibility',
+    updateProfile: config.baseUrl + 'auth/update-profile',
+    onlineVisiblity: config.baseUrl + 'auth/online-visibility',
   }
 
 };
